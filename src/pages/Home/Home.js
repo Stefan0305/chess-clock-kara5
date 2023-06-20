81ac2967c5ce4b1fe5660cf3ce9086a0175639bb
Dario <dario@kara5.mk>
2023-06-20 09:31:56 +0200

    pull

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,24 @@ export default function Home() {
 
     // const [GameModes, setGameModes] = useState(600);
 
-    return <div>
-        <div>
-        <a href="https://kara5.com"><img src="" alt="[kara5 Logo here]" /></a>
+    return <div className="wrapper">
+        <div className="blur"></div>
+        <div className="container">
+            <div className="header">
+                <a className="logo-link" href="https://kara5.com">
+                    <div className="logo"></div>
+                    <div className="logo-text"><p>CHESS</p></div>
+                </a>
+            </div>
+            <div className="button-cont">
+                <a className="home-btn" href="new-game">Start game <br /> Default: 10 min</a>
+            </div>
+            <div className="button-cont">
+                <a className="home-btn" href="leaderboard">Leaderboard</a>
+            </div>
+            <div className="button-cont">
+                <a className="home-btn" href="register">Register new player</a>
+            </div>
         </div>
-        <div><a href="new-game">Start game <br /> (default: 10 min)</a></div>
-        <div><a href="leaderboard">Leaderboard</a></div>
-        <div><a href="register">Register new player</a></div>
     </div>
 }
